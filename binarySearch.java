72cef6bd8cceeb326be56f5f12f1c822d5bc4303
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-02-04 12:39:54 +0530

    binary

---
+++ binarySearch.java
@@ -0,0 +1,28 @@
+public class binarySearch {
+    //  prerequisite - sorted array hona chahiye
+    public static int binarySearch(int arr[],int key){
+        int start = 0;
+        int end = arr.length -1;
+        while(start <=end){
+            int mid = (start+end)/2;
+
+            if(arr[mid]==key){
+                return mid;
+            }
+            else if(arr[mid]>key){
+                end = mid -1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,8,10,12,14};
+        int key = 2;
+        System.out.println(binarySearch(arr,key));
+        
+    }
+}
